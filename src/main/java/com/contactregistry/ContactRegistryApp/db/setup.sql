f814bf1811b01eb7aa3182da3c38b5645e824ba8
MugoBrian <mikanmuchiri@gmail.com>
2025-05-04 18:37:31 +0300

    Database setup scripts and Contact model.

---
+++ src/main/java/com/contactregistry/ContactRegistryApp/db/setup.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE IF NOT EXISTS contact_registry;
+USE contact_registry;
+
+CREATE TABLE IF NOT EXISTS users(
+    id INT AUTO_INCREMENT PRIMARY_KEY,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+);
+
+INSERT INTO users(username, email, password) VALUES ('admin', 'admin@example.com', MD5('admin123'));
+
+-- -- FIELDS MUST MATCH
+-- - Full Names (Will be the aggregate of first name and last name for simplicity)
+-- - Phone Number  (Can be aggregate of country code and the 10 digit number)
+-- - Email Address
+-- - ID Number
+-- - Date of Birth
+-- - Gender
+-- - County of Residence
+
+CREATE TABLE IF NOT EXISTS contacts (
+    id INT AUTO_INCREMENT PRIMARY_KEY,
+    first_name VARCHAR(100), 
+    last_name VARCHAR(100),
+    phone_number VARCHAR(20),
+    email_address VARCHAR(100),
+    id_number INT(15),
+    date_of_birth DATE,
+    gender ENUM('Male', 'Female', 'Other'),
+    county VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO contacts (full_name, phone, email, id_number, dob, gender, county)
+VALUES
+('John Doe', '+254712345678', 'john@example.com', '12345678', '1990-01-01', 'Male', 'Nairobi'),
+('Jane Smith', '+254723456789', 'jane@example.com', '87654321', '2005-05-05', 'Female', 'Mombasa');
---
+++ src/main/java/com/contactregistry/ContactRegistryApp/model/Contact.java
@@ -0,0 +1,125 @@
+package com.contactregistry.ContactRegistryApp.model;
+
+import java.time.LocalDate;
+
+// full_name  
+// phone_number 
+// email_address
+// id_number 
+// date_of_birth
+// gender     
+// county 
+
+public class Contact {
+    private int id;
+    private String first_name;
+    private String last_name;
+    private String phone_number;
+    private String email_address;
+    private int id_number;
+    private LocalDate date_of_birth;
+    private String gender;
+    private String county;
+
+    public Contact() {
+    }
+
+    public Contact(int id, String first_name, String last_name, String phone_number, String email_address,
+            int id_number,
+            LocalDate date_of_birth, String gender, String county) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.phone_number = phone_number;
+        this.email_address = email_address;
+        this.id_number = id_number;
+        this.date_of_birth = date_of_birth;
+        this.gender = gender;
+        this.county = county;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // First Name, Last Name and Full Name
+
+    public void setFirstName(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLastName(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getFullName() {
+        return this.first_name + " " + this.last_name;
+    }
+
+    public String getPhoneNumber() {
+        return this.phone_number;
+    }
+
+    public void setPhoneNumber(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getEmailAddress() {
+        return this.email_address;
+    }
+
+    public void setEmailAddress(String email_address) {
+        this.email_address = email_address;
+    }
+
+    public int getIdNumber() {
+        return this.id_number;
+    }
+
+    public void setIdNumber(int id_number) {
+        this.id_number = id_number;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return this.date_of_birth;
+    }
+
+    public void setDateOfBirth(LocalDate date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+    public String getGender() {
+        return this.gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCounty() {
+        return this.county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " id='" + getId() + "'" +
+                ", full_name='" + getFullName() + "'" +
+                ", phone_number='" + getPhoneNumber() + "'" +
+                ", email_address='" + getEmailAddress() + "'" +
+                ", id_number='" + getIdNumber() + "'" +
+                ", date_of_birth='" + getDateOfBirth() + "'" +
+                ", gender='" + getGender() + "'" +
+                ", county='" + getCounty() + "'" +
+                "}";
+    }
+
+}
