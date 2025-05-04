33221c4749ef94ffd15de90b4ff64864adbb834c
MugoBrian <mikanmuchiri@gmail.com>
2025-05-04 17:43:25 +0300

    Initial commit - project creation.

---
+++ src/main/java/com/contactregistry/ContactRegistryApp/resources/JakartaEE11Resource.java
@@ -0,0 +1,20 @@
+package com.contactregistry.ContactRegistryApp.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee11")
+public class JakartaEE11Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
